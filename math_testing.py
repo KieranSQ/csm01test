82c1152af59bd451202f85f829577f3718bb5b21
Jammyjamjamman <jdsherratt3@gmail.com>
2017-12-08 13:25:49 +0000

    added a py file which tests maths functions

---
+++ math_testing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import math 
+
+
+exponent = math.exp(3)
+logB2 = math.log2(64)
+areaCirc = math.pi*(6**2)
+
+print("Exponent:", exponent)
+print("Log2(64):", logB2)
+print("Area of circle radius 6:", areaCirc)
